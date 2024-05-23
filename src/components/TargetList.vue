850c0921d93e5af686d1e422a4c9fcc1d1d46078
GbSeason <88025830@qq.com>
2024-05-23 17:21:06 +0800

    Update TargetList.vue

--- src/components/TargetList.vue
+++ src/components/TargetList.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="frame">
+        <div v-for="(item, index) in targets" :key="index" class="target">
+            <div class="backgroundimg"></div>
+            <div class="actions">
+                <div class="button-border">执行</div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -10,13 +16,53 @@ export default {
     },
     data: function () {
         return {
-
+            targets: [{ frame: [], conf: [] },{ frame: [], conf: [] }]
         }
     }
 }
 </script>
 <style scoped>
 .frame {
-    padding: 20px 10px;
+    display: flex;
+    padding: 10px 10px 10px 0;
+    justify-content: flex-start;
+}
+.button-border{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border:1px solid #00ffff;
+    border-radius: 3px;
+    padding: 0px 2px;
+    line-height: 18px;
+    cursor: pointer;
+}
+.button-border:hover{
+    background-color: #00999e;
+}
+.actions{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 100px;
+    color: #00ffff;
+}
+.backgroundimg{
+    background-image: url('../assets/logo.png');
+    width: 100px;
+    height: 100px;
+    background-position-y: -10px;
+    background-position-x: -10px;
+    background-size: 90%;
+    background-repeat: no-repeat;
+}
+.target {
+    display: flex;
+    justify-content: flex-start;
+    border: 1px solid #d68b00;
+    width: 150px;
+    height: 100px;
+    margin-left: 10px;
 }
 </style>
